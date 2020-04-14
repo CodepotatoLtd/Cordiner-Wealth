07f23ee1783edb3ace6fafe48674bbd222f4cefe
Chris Terry <chris@codepotato.co.uk>
2020-04-14 13:57:49 +0100

    Removed blog disclaimer

--- wp-content/themes/cordinerwealth/resources/views/partials/content-single.blade.php
+++ wp-content/themes/cordinerwealth/resources/views/partials/content-single.blade.php
@@ -6,15 +6,15 @@
       </div>
       <div class="w-full lg:w-4/5 roboto-light text-grey leading-snug mx-auto pt-8 md:pt-0">
         @php the_content() @endphp
-          <div class="blog-disclaimer">
-              <p class="font-bold">Disclaimer</p><br>
+{{--          <div class="blog-disclaimer">--}}
+{{--              <p class="font-bold">Disclaimer</p><br>--}}
 
-              <p>Metis Ireland Financial Planning Ltd t/a Metis Ireland is regulated by the Central Bank of Ireland.</p><br>
+{{--              <p>Metis Ireland Financial Planning Ltd t/a Metis Ireland is regulated by the Central Bank of Ireland.</p><br>--}}
 
-              <p>All content provided in these blog posts is intended for information purposes only and should not be interpreted as financial advice. You should always engage the
-                  services of a fully qualified independent financial adviser before entering any financial contract. Metis Ireland Financial Planning Ltd t/a Metis Ireland will not be
-                  held responsible for any actions taken as a result of reading these blog posts.</p>
-          </div>
+{{--              <p>All content provided in these blog posts is intended for information purposes only and should not be interpreted as financial advice. You should always engage the--}}
+{{--                  services of a fully qualified independent financial adviser before entering any financial contract. Metis Ireland Financial Planning Ltd t/a Metis Ireland will not be--}}
+{{--                  held responsible for any actions taken as a result of reading these blog posts.</p>--}}
+{{--          </div>--}}
       </div>
     </div>
     {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
