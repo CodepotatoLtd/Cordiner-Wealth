286ac0fdf37b7d92a2c6dafa49921806c00c74c1
Chris Terry <chris@codepotato.co.uk>
2020-05-29 16:52:30 +0100

    Padding Fix

--- wp-content/themes/cordinerwealth/resources/views/partials/flex-page.blade.php
+++ wp-content/themes/cordinerwealth/resources/views/partials/flex-page.blade.php
@@ -110,7 +110,7 @@
                                 <img class="lg:ml-n100" src="@php the_sub_field('image') @endphp" alt="Cordiner Image">
                             </div>
                         </div>
-                        <div class="w-full lg:w-5/12 text-center py-16">
+                        <div class="w-full lg:w-5/12 text-center lg:px-8 py-16">
                             <div class="lg:ml-n145 md:pl-8">
                                 <h2 class="merriweather-light text-orange text-4xl leading-tight mb-4">@php the_sub_field('title') @endphp</h2>
                                 <div class="mb-6">@php the_sub_field('text') @endphp</div>
@@ -125,7 +125,7 @@
                 @if (get_sub_field('layout'))
 
                     <div class="flex flex-col-reverse lg:flex-row">
-                        <div class="w-full lg:w-1/2 text-center py-16">
+                        <div class="w-full lg:w-1/2 text-center lg:px-8 py-16">
                             <h2 class="merriweather-light text-orange text-4xl leading-tight mb-4">@php the_sub_field('title') @endphp</h2>
                             <div class="mb-6">@php the_sub_field('text') @endphp</div>
                             <a class="button white-bg-orange py-2 px-16 text-white text-center uppercase" href="@php the_sub_field('button_link') @endphp">@php the_sub_field('button_text') @endphp</a>
@@ -145,7 +145,7 @@
                                 <img src="@php the_sub_field('image') @endphp" alt="Cordiner Image">
                             </div>
                         </div>
-                        <div class="w-full lg:w-1/2 text-center py-16">
+                        <div class="w-full lg:w-1/2 text-center lg:px-8 py-16">
                             <h2 class="merriweather-light text-orange text-4xl leading-tight mb-4">@php the_sub_field('title') @endphp</h2>
                             <div class="mb-6">@php the_sub_field('text') @endphp</div>
                             <a class="button white-bg-orange py-2 px-16 text-white text-center uppercase" href="@php the_sub_field('button_link') @endphp">@php the_sub_field('button_text') @endphp</a>
